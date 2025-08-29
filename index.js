ef2205dcc74d58424d82026070597004e3225c58
Dharsha Dwarak <131132201+DHARSHADWARAK@users.noreply.github.com>
2025-08-29 11:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,5 @@ serv.post("/bfhl",(req,res)=>{
   }
 });
 
-const PORT = process.env.PORT || 3000;
-serv.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
